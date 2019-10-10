07bdea24b03fe33ce50a8fb5744ea46027ea3ca8
phausamann <peter@hausamann.de>
2019-10-10 13:19:56 -0700

    Fix bug in setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     include_package_data=True,
     keywords='xrview',
     name='xrview',
-    packages=find_packages(include=['xrview']),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
