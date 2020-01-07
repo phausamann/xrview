b8b1be765c36c2813e19ecbe0749f1354cfefcf0
phausamann <peter.hausamann@tum.de>
2020-01-07 18:03:21 +0100

    Update Element constructor

--- xrview/timeseries.py
+++ xrview/timeseries.py
@@ -170,7 +170,6 @@ class TimeseriesViewer(BaseViewer):
             The number of points to render for each pixel of this figure. If
             not specified, the resolution of the viewer is used.
         """
-        glyphs = get_glyph_list(glyphs)
         element = self.element_type(glyphs, data, coords, name, resolution)
         self.added_figures.append(element)
 
@@ -202,7 +201,6 @@ class TimeseriesViewer(BaseViewer):
             The number of points to render for each pixel of this figure. If
             not specified, the resolution of the viewer is used.
         """
-        glyphs = get_glyph_list(glyphs)
         element = self.element_type(glyphs, data, coords, name, resolution)
         self.added_overlays.append(element)
         self.added_overlay_figures.append(onto)
