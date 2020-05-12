6aee4aaaa1012eabb8586d7e53a5eed4fcd27475
Peter Hausamann <peter.hausamann@tum.de>
2020-05-12 17:16:48 +0200

    Update conda_upload.sh

--- ci/conda_recipe/conda_upload.sh
+++ ci/conda_recipe/conda_upload.sh
@@ -3,4 +3,6 @@
 PKG_NAME=xrview
 USER=phausamann
 
+source "$HOME/miniconda/etc/profile.d/conda.sh"
+conda activate base
 anaconda -t $ANACONDA_TOKEN upload -u $USER $CONDA_BLD_PATH/noarch/$PKG_NAME*.tar.bz2 --force
